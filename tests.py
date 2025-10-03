917b73868fb3a5878b1e7129f4e5d6c52e1ec64c
ravenfury2 <ravenfury2@gmail.com>
2025-10-02 20:31:08 -0400

    Added function to be able to write files. Modified test to test it out.

--- tests.py
+++ tests.py
@@ -1,16 +1,13 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
-    result = get_file_content("calculator", "main.py")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_file_content("calculator", "/bin/cat")
-    print(result)
-
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
 
